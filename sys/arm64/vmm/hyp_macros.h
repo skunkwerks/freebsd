c8591a589851bc768ca431f5404423ef42e9d16a
Alexandru Elisei <alexandru.elisei@gmail.com>
2017-08-14 11:41:42 +0300

    Save and load guest registers on a world switch

--- sys/arm64/vmm/hyp_macros.h
+++ sys/arm64/vmm/hyp_macros.h
@@ -27,76 +27,15 @@
 #ifndef _VMM_HYP_MACROS_H_
 #define	_VMM_HYP_MACROS_H_
 
-/*
- * The STR instruction takes an offset between [-256, 255], but the hypctx
- * register offset can be larger than that.
- *
- * Use a temporary register to hold the offset.
- */
-#define	SAVE_SYSTEM_REG32(reg)				\
-	mrs	x9, reg;				\
-	mov	x10, #HYPCTX_##reg;			\
-	str	w9, [x0, x10];
-
-#define LOAD_SYSTEM_REG32(reg)				\
-	mov	x9, #HYPCTX_##reg;			\
-	ldr	w10, [x0, x9];				\
-	msr	reg, x10;
-
-#define	SAVE_SYSTEM_REG64(reg)				\
-	mrs	x9, reg;				\
-	mov	x10, #HYPCTX_##reg;			\
-	str	x9, [x0, x10];
-
-#define LOAD_SYSTEM_REG64(reg)				\
-	mov	x9, #HYPCTX_##reg;			\
-	ldr	x10, [x0, x9];				\
-	msr	reg, x10;
-
 #define PUSH_SYSTEM_REG_PAIR(reg0, reg1)		\
-	mrs	x9, reg0;				\
-	mrs	x10, reg1;				\
-	stp	x10, x9, [sp, #-16]!;
+	mrs	x1, reg0;				\
+	mrs	x2, reg1;				\
+	stp	x2, x1, [sp, #-16]!;
 
 #define POP_SYSTEM_REG_PAIR(reg0, reg1)			\
-	ldp	x10, x9, [sp], #16;			\
-	msr	reg1, x10;				\
-	msr	reg0, x9;
-
-#define	SAVE_REG(reg)					\
-	mov	x9, #HYPCTX_REGS_##reg;			\
-	str	reg, [x0, x9];
-
-#define	LOAD_REG(reg)					\
-	mov	x9, #HYPCTX_REGS_##reg;			\
-	ldr	reg, [x0, x9];
-
-/*
- * Save a pair of consecutive registers.
- *
- * The STP instruction takes an immediate in the range of [-512, 504] when
- * using the post-indexed addressing mode, but the hypctx register offset can be
- * larger than that.
- *
- * Compute the address by adding the hypctx base address with the register
- * member offset.
- *
- * Using STP/LDP to save/load register pairs to the corresponding struct hypctx
- * variables works because the registers are declared as an array and they are
- * stored in contiguous memory addresses.
- */
-#define	SAVE_REG_PAIR(reg0, reg1)			\
-	mov	x9, #HYPCTX_REGS_##reg0;		\
-	add	x9, x0, x9;				\
-	stp	reg0, reg1, [x9];
-
-/*
- * Load a pair of consecutive registers.
- */
-#define LOAD_REG_PAIR(reg0, reg1)			\
-	mov	x9, #HYPCTX_REGS_##reg0;		\
-	add	x9, x0, x9;				\
-	ldp	reg0, reg1, [x9];
+	ldp	x2, x1, [sp], #16;			\
+	msr	reg1, x2;				\
+	msr	reg0, x1;
 
 /*
  * Push all the host registers before returning to the guest.
@@ -176,22 +115,55 @@
 	/* Restore hypctx address from TPIDR_EL2 */	\
 	mrs	x0, tpidr_el2;
 
+#define	SAVE_REG(reg)					\
+	mov	x1, #HYPCTX_REGS_##reg;			\
+	str	reg, [x0, x1];
+
+#define	LOAD_REG(reg)					\
+	mov	x1, #HYPCTX_REGS_##reg;			\
+	ldr	reg, [x0, x1];
+
 /*
- * Save all the guest registers.
+ * The STP and LDP instructions takes an immediate in the range of [-512, 504]
+ * when using the post-indexed addressing mode, but the hypctx register offset
+ * can be larger than that. To get around this limitation we compute the address
+ * by adding the hypctx base address with the struct member offset.
  *
- * Expecting:
- * x0 - struct hypctx address
+ * Using STP/LDP to save/load register pairs to the corresponding struct hypctx
+ * variables works because the registers are declared as an array and they are
+ * stored in contiguous memory addresses.
  */
-#define	SAVE_GUEST_REGS()				\
-	/* Save regular registers first */		\
-	/* We use x0 for the hypctx address, save it on the stack */	\
+
+#define	SAVE_REG_PAIR(reg0, reg1)			\
+	mov	x1, #HYPCTX_REGS_##reg0;		\
+	add	x1, x0, x1;				\
+	stp	reg0, reg1, [x1];
+
+#define LOAD_REG_PAIR(reg0, reg1)			\
+	mov	x1, #HYPCTX_REGS_##reg0;		\
+	add	x1, x0, x1;				\
+	ldp	reg0, reg1, [x1];
+
+/*
+ * We use x0 to load the hypctx address from TPIDR_EL2 and x1 and x2 as
+ * temporary registers to compute the hypctx member addresses. To save the guest
+ * values at first we push them on the stack, use these temporary registers to
+ * save the rest of the registers and at the end we pop the values from the
+ * stack and save them.
+ */
+#define SAVE_GUEST_X_REGS()				\
+	/* Push x0 */					\
 	str	x0, [sp, #-16]!;			\
-	/* We use x9 and x10 as temporary registers, push them both */	\
-	stp	x9, x10, [sp, #-16]!;			\
-	SAVE_REG_PAIR(X1, X2);				\
+	/* Restore hypctx address */			\
+	mrs	x0, tpidr_el2;				\
+	/* Push x1 and x2 */				\
+	stp	x1, x2, [sp, #-16]!;			\
+							\
+	/* Save the other registers */			\
 	SAVE_REG_PAIR(X3, X4);				\
 	SAVE_REG_PAIR(X5, X6);				\
 	SAVE_REG_PAIR(X7, X8);				\
+	SAVE_REG_PAIR(X9, X10);				\
 	SAVE_REG_PAIR(X11, X12);			\
 	SAVE_REG_PAIR(X13, X14);			\
 	SAVE_REG_PAIR(X15, X16);			\
@@ -202,5 +174,142 @@
 	SAVE_REG_PAIR(X25, X26);			\
 	SAVE_REG_PAIR(X27, X28);			\
 	SAVE_REG(X29);					\
+							\
+	/* Pop and save x1 and x2 */			\
+	ldp	x1, x2, [sp], #16;			\
+	mov	x3, #HYPCTX_REGS_X1;			\
+	add	x3, x0, x3;				\
+	stp	x1, x2, [x3];				\
+	/* Pop and save x0 */				\
+	ldr	x1, [sp], #16;				\
+	mov	x2, #HYPCTX_REGS_X0;			\
+	str	x1, [x0, x2];
+
+/*
+ * The STR and LDR instructions take an offset between [-256, 255], but the
+ * hypctx register offset can be larger than that. To get around this limitation
+ * we use a temporary register to hold the offset.
+ */
+#define	SAVE_SYSTEM_REG32(reg)				\
+	mrs	x1, reg;				\
+	mov	x2, #HYPCTX_##reg;			\
+	str	w1, [x0, x2];
+
+#define	SAVE_SYSTEM_REG64(reg)				\
+	mrs	x1, reg;				\
+	mov	x2, #HYPCTX_##reg;			\
+	str	x1, [x0, x2];
+
+#define LOAD_SYSTEM_REG32(reg)				\
+	mov	x1, #HYPCTX_##reg;			\
+	ldr	w2, [x0, x1];				\
+	msr	reg, x2;
+
+#define LOAD_SYSTEM_REG64(reg)				\
+	mov	x1, #HYPCTX_##reg;			\
+	ldr	x2, [x0, x1];				\
+	msr	reg, x2;
+
+/*
+ * Save all the guest registers. Start by saving the regular registers first
+ * because those will be used as temporary registers for accessing the hypctx
+ * member addresses.
+ *
+ * Expecting:
+ * TPIDR_EL2 - struct hypctx address
+ */
+#define	SAVE_GUEST_REGS()				\
+	SAVE_GUEST_X_REGS();				\
+							\
+	SAVE_SYSTEM_REG64(ACTLR_EL1);			\
+	SAVE_SYSTEM_REG64(AMAIR_EL1);			\
+	SAVE_SYSTEM_REG64(ELR_EL1);			\
+	SAVE_SYSTEM_REG64(PAR_EL1);			\
+	SAVE_SYSTEM_REG64(MAIR_EL1);			\
+	SAVE_SYSTEM_REG64(TCR_EL1);			\
+	SAVE_SYSTEM_REG64(TPIDR_EL0);			\
+	SAVE_SYSTEM_REG64(TPIDR_EL1);			\
+	SAVE_SYSTEM_REG64(TPIDRRO_EL0);			\
+	SAVE_SYSTEM_REG64(TTBR0_EL1);			\
+	SAVE_SYSTEM_REG64(TTBR1_EL1);			\
+	SAVE_SYSTEM_REG64(VBAR_EL1);			\
+							\
+	SAVE_SYSTEM_REG32(AFSR0_EL1);			\
+	SAVE_SYSTEM_REG32(AFSR1_EL1);			\
+	SAVE_SYSTEM_REG32(CONTEXTIDR_EL1);		\
+	SAVE_SYSTEM_REG32(CPACR_EL1);			\
+	SAVE_SYSTEM_REG32(ESR_EL1);			\
+	SAVE_SYSTEM_REG32(FAR_EL1);			\
+	SAVE_SYSTEM_REG32(SCTLR_EL1);			\
+	SAVE_SYSTEM_REG32(SPSR_EL1);
+
+/*
+ * We use x1 as a temporary register to store the hypctx member offset and x0
+ * to hold the hypctx address. We load the x0 and x1 guest values, push them on
+ * the stack and after all the other registers have been restored we pop and
+ * restore them.
+ */
+#define	LOAD_GUEST_X_REGS()				\
+	/* Load and push on the stack guest x0 and x1 */\
+	mov	x1, #HYPCTX_REGS_X0;			\
+	add	x1, x1, x0;				\
+	/* Make x2 = x0 and x3 = x1 */			\
+	ldp	x2, x3, [x1];				\
+	stp	x2, x3, [sp, #-16]!;			\
+							\
+	/* Load the other registers */			\
+	LOAD_REG_PAIR(X2, X3);				\
+	LOAD_REG_PAIR(X4, X5);				\
+	LOAD_REG_PAIR(X6, X7);				\
+	LOAD_REG_PAIR(X8, X9);				\
+	LOAD_REG_PAIR(X10, X11);			\
+	LOAD_REG_PAIR(X12, X13);			\
+	LOAD_REG_PAIR(X14, X15);			\
+	LOAD_REG_PAIR(X16, X17);			\
+	LOAD_REG_PAIR(X18, X19);			\
+	LOAD_REG_PAIR(X20, X21);			\
+	LOAD_REG_PAIR(X22, X23);			\
+	LOAD_REG_PAIR(X24, X25);			\
+	LOAD_REG_PAIR(X26, X27);			\
+	LOAD_REG_PAIR(X28, X29);			\
+							\
+	/* Save hypctx address to tpidr_el2 */		\
+	msr	tpidr_el2, x0;				\
+	/* Pop guest x0 and x1 from the stack */	\
+	ldp	x0, x1, [sp], #16;			\
+
+/*
+ * Restore all the guest registers to their original values.
+ *
+ * Expecting:
+ * x0 - struct hypctx address
+ *
+ * After call:
+ * tpidr_el2 - struct hypctx address
+ */
+#define	LOAD_GUEST_REGS()				\
+	LOAD_SYSTEM_REG64(ACTLR_EL1);			\
+	LOAD_SYSTEM_REG64(AMAIR_EL1);			\
+	LOAD_SYSTEM_REG64(ELR_EL1);			\
+	LOAD_SYSTEM_REG64(PAR_EL1);			\
+	LOAD_SYSTEM_REG64(MAIR_EL1);			\
+	LOAD_SYSTEM_REG64(TCR_EL1);			\
+	LOAD_SYSTEM_REG64(TPIDR_EL0);			\
+	LOAD_SYSTEM_REG64(TPIDR_EL1);			\
+	LOAD_SYSTEM_REG64(TPIDRRO_EL0);			\
+	LOAD_SYSTEM_REG64(TTBR0_EL1);			\
+	LOAD_SYSTEM_REG64(TTBR1_EL1);			\
+	LOAD_SYSTEM_REG64(VBAR_EL1);			\
+							\
+	LOAD_SYSTEM_REG32(AFSR0_EL1);			\
+	LOAD_SYSTEM_REG32(AFSR1_EL1);			\
+	LOAD_SYSTEM_REG32(CONTEXTIDR_EL1);		\
+	LOAD_SYSTEM_REG32(CPACR_EL1);			\
+	LOAD_SYSTEM_REG32(ESR_EL1);			\
+	LOAD_SYSTEM_REG32(FAR_EL1);			\
+	LOAD_SYSTEM_REG32(SCTLR_EL1);			\
+	LOAD_SYSTEM_REG32(SPSR_EL1);			\
+							\
+	LOAD_GUEST_X_REGS();
 
 #endif /* !_VMM_HYP_MACROS_H_ */
