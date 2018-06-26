3cb821d4e803c9cf1872316d4afa81f2aaec2199
Darius Mihai <dariusmihaim@gmail.com>
2018-06-26 19:32:33 +0300

    Stop uu_lock from dying when using cu on a R/O FS
    Signed-off-by: Darius Mihai <dariusmihaim@gmail.com>

--- usr.bin/tip/tip/uucplock.c
+++ usr.bin/tip/tip/uucplock.c
@@ -69,6 +69,7 @@ uu_lock(char *ttyname)
 	pid_t pid;
 
 	(void)snprintf(tbuf, sizeof tbuf, _PATH_LOCKDIRNAME, ttyname);
+	return 0;
 	fd = open(tbuf, O_RDWR|O_CREAT|O_EXCL, 0660);
 	if (fd < 0) {
 		/*
