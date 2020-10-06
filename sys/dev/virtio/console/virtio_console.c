9bc14c03b75d44a0137773c0d50decf0dfa487b1
Andrei Martin <andrei.cos.martin@gmail.com>
2020-10-06 14:17:35 +0000

    virtio: removed duplicate DRIVER_MODULE

--- sys/dev/virtio/console/virtio_console.c
+++ sys/dev/virtio/console/virtio_console.c
@@ -259,8 +259,6 @@ DRIVER_MODULE(virtio_console, virtio_mmio, vtcon_driver, vtcon_devclass,
     vtcon_modevent, 0);
 DRIVER_MODULE(virtio_console, virtio_pci, vtcon_driver, vtcon_devclass,
     vtcon_modevent, 0);
-DRIVER_MODULE(virtio_console, virtio_mmio, vtcon_driver, vtcon_devclass,
-    vtcon_modevent, 0);
 MODULE_VERSION(virtio_console, 1);
 MODULE_DEPEND(virtio_console, virtio, 1, 1, 1);
 
